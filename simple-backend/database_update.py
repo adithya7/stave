60e8fc79bfda8a1980179819ff811240b9c109b2
132lilinwei <llw19970903@live.com>
2020-12-21 21:23:24 -0500

    db update

--- simple-backend/database_update.py
+++ simple-backend/database_update.py
@@ -1,12 +1,13 @@
-from forte.data.multi_pack import MultiPack
+# from forte.data.multi_pack import MultiPack
 import sqlite3
 import json
-from utils import gen_hash
+from nlpviewer_backend.utils import gen_hash
 def add_pack(name, textPack, ontology, packID):
 	"""
-	pack must be a string of json
+	textPack must be a string of json
+	ontology must be a string of json
 	"""
-	con = sqlite3.connect('simple-backend/db.sqlite3')
+	con = sqlite3.connect('db.sqlite3')
 	cursor = con.cursor()
 	textPack = json.dumps(textPack)
 	ontology = json.dumps(ontology)
@@ -15,14 +16,18 @@ def add_pack(name, textPack, ontology, packID):
 	cursor.execute("INSERT INTO nlpviewer_backend_document(textPack, ontology, packID, name) \
 	            VALUES(?,?,?,?)",(textPack, ontology, packID, name))
 
-def add_multipack(name, textPack, ontology, packID, idHash):
-	con = sqlite3.connect('simple-backend/db.sqlite3')
+def add_multipack(name, textPack, ontology, packID):
+	"""
+	textPack must be a string of json
+	ontology must be a string of json
+	"""
+	con = sqlite3.connect('db.sqlite3')
 	cursor = con.cursor()
 	textPack = json.dumps(textPack)
 	ontology = json.dumps(ontology)
 	idHash = gen_hash(name)
 	cursor.execute("INSERT INTO nlpviewer_backend_crossdoc(textPack, ontology, packID, name, idHash) \
-            VALUES(?,?,?,?)",(textPack, ontology, packID, name, idHash))
+            VALUES(?,?,?,?,?)",(textPack, ontology, packID, name, idHash))
 	return idHash
 
 
