9d116fb18e7c3450913d4e16ef092453284ef49d
132lilinwei <llw19970903@live.com>
2020-12-21 21:05:16 -0500

    return hash in forte_database_update

--- simple-backend/database_update.py
+++ simple-backend/database_update.py
@@ -1,3 +1,4 @@
+from forte.data.multi_pack import MultiPack
 import sqlite3
 import json
 from utils import gen_hash
@@ -22,5 +23,10 @@ def add_multipack(name, textPack, ontology, packID, idHash):
 	idHash = gen_hash(name)
 	cursor.execute("INSERT INTO nlpviewer_backend_crossdoc(textPack, ontology, packID, name, idHash) \
             VALUES(?,?,?,?)",(textPack, ontology, packID, name, idHash))
+	return idHash
+
+
+# def forte_add_multipack(input_pack: MultiPack):
+# 	add_multipack(input_pack.pack_name, input_pack)
 
 
